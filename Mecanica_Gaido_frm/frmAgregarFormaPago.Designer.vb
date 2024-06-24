46953bc246a359f36e0614368d29c8348f604088
rocioluque <rocioluq4@gmail.com>
2024-06-24 13:07:55 -0300

    Cambios Varios

---
+++ Mecanica_Gaido_frm/frmAgregarFormaPago.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmAgregarFormaPago
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmAgregarFormaPago))
+        Me.btnCerrar = New System.Windows.Forms.PictureBox()
+        Me.txtFormaPago = New System.Windows.Forms.TextBox()
+        Me.lblRubro = New System.Windows.Forms.Label()
+        Me.btnAgregar = New System.Windows.Forms.Button()
+        CType(Me.btnCerrar, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnCerrar
+        '
+        Me.btnCerrar.BackgroundImage = CType(resources.GetObject("btnCerrar.BackgroundImage"), System.Drawing.Image)
+        Me.btnCerrar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.btnCerrar.Location = New System.Drawing.Point(364, 1)
+        Me.btnCerrar.Name = "btnCerrar"
+        Me.btnCerrar.Size = New System.Drawing.Size(19, 18)
+        Me.btnCerrar.TabIndex = 22
+        Me.btnCerrar.TabStop = False
+        '
+        'txtFormaPago
+        '
+        Me.txtFormaPago.Font = New System.Drawing.Font("Century Gothic", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtFormaPago.Location = New System.Drawing.Point(29, 93)
+        Me.txtFormaPago.Name = "txtFormaPago"
+        Me.txtFormaPago.Size = New System.Drawing.Size(228, 21)
+        Me.txtFormaPago.TabIndex = 21
+        '
+        'lblRubro
+        '
+        Me.lblRubro.AutoSize = True
+        Me.lblRubro.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblRubro.ForeColor = System.Drawing.Color.White
+        Me.lblRubro.Location = New System.Drawing.Point(25, 58)
+        Me.lblRubro.Name = "lblRubro"
+        Me.lblRubro.Size = New System.Drawing.Size(121, 20)
+        Me.lblRubro.TabIndex = 20
+        Me.lblRubro.Text = "Forma de Pago"
+        '
+        'btnAgregar
+        '
+        Me.btnAgregar.FlatAppearance.BorderColor = System.Drawing.Color.SeaGreen
+        Me.btnAgregar.FlatAppearance.BorderSize = 2
+        Me.btnAgregar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.MediumSeaGreen
+        Me.btnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnAgregar.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAgregar.ForeColor = System.Drawing.Color.White
+        Me.btnAgregar.Location = New System.Drawing.Point(291, 160)
+        Me.btnAgregar.Name = "btnAgregar"
+        Me.btnAgregar.Size = New System.Drawing.Size(82, 29)
+        Me.btnAgregar.TabIndex = 19
+        Me.btnAgregar.Text = "Agregar"
+        Me.btnAgregar.UseVisualStyleBackColor = True
+        '
+        'frmAgregarFormaPago
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(65, Byte), Integer), CType(CType(65, Byte), Integer), CType(CType(65, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(385, 201)
+        Me.Controls.Add(Me.btnCerrar)
+        Me.Controls.Add(Me.txtFormaPago)
+        Me.Controls.Add(Me.lblRubro)
+        Me.Controls.Add(Me.btnAgregar)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmAgregarFormaPago"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "AgregarTipoPago"
+        CType(Me.btnCerrar, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnCerrar As PictureBox
+    Friend WithEvents txtFormaPago As TextBox
+    Friend WithEvents lblRubro As Label
+    Friend WithEvents btnAgregar As Button
+End Class
